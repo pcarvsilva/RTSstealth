fe38b5d58a4d7863b1966e53edd17797d2d6bf7b
pcarvsilva <pcarvsilva@gmail.com>
2018-02-08 19:45:50 -0200

    First Commit

---
+++ Assets/Functions/ToolbarActions.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum ToolbarAction
+{
+    Move,
+    Attack
+}
+
+public class ToolbarActions : MonoBehaviour {
+
+    public NavMeshAgent agent;
+    public ToolbarAction action;
+
+    // Use this for initialization
+    void Start() {
+        switch (action) {
+            case (ToolbarAction.Move):
+                {
+                    GetComponent<ToolbarElement>().ClickFunction.AddListener(Move);
+                    break;
+                }
+            case (ToolbarAction.Attack):
+                {
+                    GetComponent<ToolbarElement>().ClickFunction.AddListener(Attack);
+                    break;
+                }
+        }
+	}
+
+
+    private void Move(RaycastHit hit)
+    {
+        StartCoroutine(move(hit.point));
+    }
+
+    private void Attack(RaycastHit hit)
+    {
+        StartCoroutine(attack(hit.collider.gameObject));
+    }
+
+    private IEnumerator move(Vector3 point)
+    {
+        agent.destination = point;
+        yield return null;
+        yield return new WaitAgentReachDestination(agent);
+    }
+
+    private IEnumerator follow(Transform transform)
+    {
+        do
+        {
+            agent.destination = transform.position;
+            yield return null;
+        } while (agent.remainingDistance > agent.stoppingDistance);
+    }
+
+    private IEnumerator attack(GameObject attacked)
+    {
+        yield return StartCoroutine(follow(attacked.transform));
+        Destroy(attacked);
+    }
+}
---
+++ Assets/Toolbar/ToolbarCursor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.AI;
+
+public class ToolbarCursor : MonoBehaviour {
+
+
+    public ToolbarEvent OperationOnClick;
+    public LayerMask layer;
+
+    public Coroutine currentRoutine;
+
+    private void Start()
+    {
+        GetComponentInChildren<ToolbarElement>().AssignToToolbar(gameObject);
+    }
+
+
+    void Update () {
+		if(Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            resolveCallbacks();
+        }
+	}
+
+    void resolveCallbacks()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit,layer))
+        {
+            if(currentRoutine != null)
+                StopCoroutine(currentRoutine);
+            OperationOnClick.Invoke(hit);
+        }
+    }
+}
