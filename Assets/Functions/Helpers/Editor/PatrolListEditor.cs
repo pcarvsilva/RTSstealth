d0f3245a88566013ed8e443df46f7ef7b9159ccf
pcarvsilva <pcarvsilva@gmail.com>
2018-03-14 20:57:12 -0300

    Patrol System

---
+++ Assets/Functions/Helpers/Editor/PatrolListEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(PatrolList))]
+public class PatrolListEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
+        if (Event.current.type == EventType.MouseDown)
+        {
+            Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                PatrolList list = (PatrolList)target;
+                list.waypoints.Add(hit.point);
+            }
+        }
+
+        PatrolList patrol = (PatrolList)target;
+        for (int i = 0; i < patrol.waypoints.Count; i++)
+        {
+            Handles.Label(patrol.waypoints[i], (i+1).ToString()); ;
+        }
+    }
+}
---
+++ Assets/Functions/Helpers/PatrolList.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AITools;
+
+
+public class PatrolList : MonoBehaviour {
+
+    public List<Vector3> waypoints;
+    public string listToInsert;
+
+	// Use this for initialization
+	void Start () {
+        List<object> points = new List<object>();
+        foreach (Vector3 t in waypoints) { points.Add(t); };
+        GetComponent<BehaviourTreeAgent>().listParameters[listToInsert] = points;
+	}
+
+}
