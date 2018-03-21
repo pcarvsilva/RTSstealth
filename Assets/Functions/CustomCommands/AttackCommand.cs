f45d821181c4043b9764d14ae79c68b191d72768
pcarvsilva <pcarvsilva@gmail.com>
2018-03-21 17:27:19 -0300

    Better folder hierarchy and easy install

---
+++ Assets/Functions/CustomCommands/AttackCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AITools;
+
+public class AttackCommand : ICommand
+{
+    public void OnClick(GameObject agent, RaycastHit hit)
+    {
+        BehaviourTreeAgent treeAgent = agent.GetComponent<BehaviourTreeAgent>();
+        treeAgent.boolParameters["Attack"] = true;
+        treeAgent.boolParameters["Move"] = false;
+        treeAgent.boolParameters["Interact"] = false;
+
+        treeAgent.gameObjectParameters["Follow"] = hit.collider.gameObject;
+
+        treeAgent.Refresh();
+    }
+}
---
+++ Assets/Functions/CustomCommands/SelectionCommand.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionCommand : ICommand
+{
+    public void OnClick(GameObject agent, RaycastHit hit)
+    {
+        CommandController.instance.selectAgent(hit.collider.gameObject);
+    }
+}
