064671ff32f9a4f3e6ac2edb764c54bb4914bfd6
Pedro <pcarvsilva@gmail.com>
2019-07-23 18:33:46 -0300

    Animations and AI

---
+++ Assets/NavMeshAnimationIntegration.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshAnimationIntegration : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Animator animator;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float speed = agent.velocity.magnitude;
+
+        //NormalizeSpeed
+        speed = speed /agent.speed;
+
+        animator.SetFloat("Speed",speed);
+    }
+}
